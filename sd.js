55817f873910c89ed24842e185d87018bcf9e076
adexp01 <andreydexp01@gmail.com>
2022-10-19 15:06:11 +0300

    test2

---
+++ sd.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Sd = () => {
+    return (
+        <div>
+                dasdfasdf
+            asdfasdf
+
+            dfas
+            fa
+            sd
+            f
+            as
+            df
+            as
+            df
+
+            asd
+
+
+        </div>
+    );
+};
+
+export default Sd;
